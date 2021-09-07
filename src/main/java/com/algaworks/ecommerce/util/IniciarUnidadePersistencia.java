98af672822968c006304bab5d965f2c30b64e468
jairomfjr <jairomfjr@gmail.com>
2021-09-07 19:07:54 -0300

    Configuração do entityManager e entityManagerFactory

---
+++ src/main/java/com/algaworks/ecommerce/util/IniciarUnidadePersistencia.java
@@ -0,0 +1,23 @@
+package com.algaworks.ecommerce.util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.algaworks.ecommerce.model.Produto;
+
+public class IniciarUnidadePersistencia {
+
+	public static void main(String[] args) {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Ecommerce-PU");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		// Testes aqui
+		Produto produto = entityManager.find(Produto.class, 1);
+		System.out.println(produto.getNome());
+		
+		
+		entityManager.close();
+		entityManagerFactory.close();
+	}
+}
